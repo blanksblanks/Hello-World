0000c9c47821a2b419ef03fda38701e3adec6a0c
Nina Baculinao <nb2406@columbia.edu>
2014-05-04 11:15:38 +0800

    Add more strings

--- HeyThere.java
+++ HeyThere.java
@@ -3,6 +3,13 @@ public class HeyThere
 	public static void main (String[] args)
 	{
 	System.out.println("HEY NiNA~!!");
+        System.out.println("...6 years late...");
+        System.out.println("\"Hello World\" tradition,");
+        System.out.println("Forgive my juvenile-ness.");
+        System.out.println("I was only 19.");
+        System.out.println("I was not too cool.\n" +
+                           "Accept this double haiku.\n" +
+                           "Thanks, sorry, good-bye!");
 	}
 }
 
